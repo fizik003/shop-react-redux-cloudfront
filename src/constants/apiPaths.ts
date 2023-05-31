7d57ddea5ade8ece5c216f8cb3e1500af441f5a8
Yury Barysevich1 <kassik0boks@gmail.com>
2023-05-31 13:39:23 +0300

    finish task 8

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://ui1nwqy5a9.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://0g4d7q8i35.execute-api.us-east-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://gjtlcp8qc2.execute-api.us-east-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
